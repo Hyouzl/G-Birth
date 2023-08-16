5e7f78c903cefc36e400653f5c4474c10343a836
rong5026 <rong5026@naver.com>
2023-08-16 16:07:49 +0900

    init

---
+++ src/main/java/com/shop/gbirth/GBirthApplication.java
@@ -0,0 +1,13 @@
+package com.shop.gbirth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBirthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GBirthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/gbirth/GBirthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.gbirth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GBirthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
